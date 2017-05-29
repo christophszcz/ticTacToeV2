a9ab5893f35d9f4973a6b2cf74d9361c8d0f5cf5
christophszcz <christoph.szczechowicz@gmail.com>
2017-05-29 15:42:20 -0400

    Stop the computer from flooding the board with inputs.

--- js/script.js
+++ js/script.js
@@ -32,6 +32,7 @@ if(User.turn){
 			$( "#box1" ).append(User.selection);
 			$( "#box1" ).attr("value", User.selection);
 		}
+		$( "#box1" ).off('click');
 	});
 
 	$('#box2').click(function(){
@@ -39,6 +40,7 @@ if(User.turn){
 			$( "#box2" ).append( User.selection);
 			$( "#box2" ).attr("value", User.selection);
 		}
+		$( "#box2" ).off('click');
 	});
 
 	$('#box3').click(function(){
@@ -46,6 +48,7 @@ if(User.turn){
 			$( "#box3" ).append( User.selection);
 			$( "#box3" ).attr("value", User.selection);
 		}
+		$( "#box3" ).off('click');
 	});
 
 	$('#box4').click(function(){
@@ -53,6 +56,7 @@ if(User.turn){
 			$( "#box4" ).append( User.selection);
 			$( "#box4" ).attr("value", User.selection);
 		}
+		$( "#box4" ).off('click');
 	});
 
 	$('#box5').click(function(){
@@ -60,6 +64,7 @@ if(User.turn){
 			$( "#box5" ).append( User.selection);
 			$( "#box5" ).attr("value", User.selection);
 		}
+		$( "#box5" ).off('click');
 	});
 
 	$('#box6').click(function(){
@@ -67,6 +72,7 @@ if(User.turn){
 			$( "#box6" ).append( User.selection);
 			$( "#box6" ).attr("value", User.selection);
 		}
+		$( "#box6" ).off('click');
 	});
 
 	$('#box7').click(function(){
@@ -74,6 +80,7 @@ if(User.turn){
 			$( "#box7" ).append( User.selection);
 			$( "#box7" ).attr("value", User.selection);
 		}
+		$( "#box7" ).off('click');
 	});
 
 	$('#box8').click(function(){
@@ -81,6 +88,7 @@ if(User.turn){
 			$( "#box8" ).append( User.selection);
 			$( "#box8" ).attr("value", User.selection);
 		}
+		$( "#box8" ).off('click');
 	});
 
 	$('#box9').click(function(){
@@ -88,10 +96,12 @@ if(User.turn){
 			$( "#box9" ).append( User.selection);
 			$( "#box9" ).attr("value", User.selection);
 		}
+		$( "#box9" ).off('click');
 	});
-
 	User.turn = false;
 	Computer.turn = true;
+	// checkIfWin();
+	//win game if 3 in a horizontal row are the same
 }
  
 //A.I. Algorithm
