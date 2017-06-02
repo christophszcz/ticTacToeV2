264daeed821b9c953bf2dada9ea32d0fe4b4ca4c
christophszcz <christoph.szczechowicz@gmail.com>
2017-06-02 16:19:51 -0400

    Display the winner symbol in the speech bubble.

--- js/winningCombination.js
+++ js/winningCombination.js
@@ -2,10 +2,13 @@
 
 function checkIfWin(){
 	if( !($('#box1').is(':empty'))  &&  $("#box1").attr("value") ===  $("#box4").attr("value")  &&  ($("#box4").attr("value") ===  $("#box7").attr("value")) ){
-		alert($("#box4").attr("value") + ' has won the game!');
+		document.getElementById('speech-bubble-id').insertAdjacentHTML("afterbegin", $("#box1").attr("value") + ' has won the game!');
 		endTheGame();
 	} else if( !($('#box2').is(':empty'))  &&  $("#box2").attr("value") ===  $("#box5").attr("value")  &&  ($("#box5").attr("value") ===  $("#box8").attr("value")) ){
-		alert($("#box5").attr("value") + ' has won the game!');
+		document.getElementById('speech-bubble-id').insertAdjacentHTML("afterbegin", $("#box2").attr("value") + ' has won the game!');
+		endTheGame();
+	} else if( !($('#box3').is(':empty'))  &&  $("#box3").attr("value") ===  $("#box6").attr("value")  &&  ($("#box6").attr("value") ===  $("#box9").attr("value")) ){
+		document.getElementById('speech-bubble-id').insertAdjacentHTML("afterbegin", $("#box3").attr("value") + ' has won the game!');
 		endTheGame();
 	}
 }
